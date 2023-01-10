9da66adf2346979269e3d5763ba246a45e0a161f
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2023-01-09 22:58:49 -0300

    added ground sprite and .gitignore

--- game.ts
+++ game.ts
@@ -1,9 +1,16 @@
-import { Player } from "./player.js";
-import { Sprites } from "./sprites.js";
+import { Background } from "./js/classes/Background.js";
+import { CollisionBlockData } from "./js/classes/CollisionBlocksData.js";
+import { Controller } from "./js/classes/Controller.js";
+import { Player } from "./js/classes/Player.js";
+import { Sprites } from "./js/classes/Sprites.js";
+import { CollisionBlock } from "./js/types/collisionBlock.js";
+// import { CollisionBlockData } from "./js/classes/CollisionBlocksData.js";
+// import { CollisionBlock } from "./js/types/collisionBlock.js";
+import { Util } from "./js/utils.js";
 
 let canvas = document.querySelector("canvas");
 let contextDefinition = (): CanvasRenderingContext2D => {
-  const canvasWidth = 860;
+  const canvasWidth = 864;
   const canvasHeight = 480;
 
   if (canvas === null) throw new Error("Canvas is null");
@@ -18,89 +25,107 @@ let contextDefinition = (): CanvasRenderingContext2D => {
 };
 
 const ctx = contextDefinition();
-let background: Sprites;
+let background: Background;
+let ground: Background;
+let shop: Sprites;
 let player1: Player;
+let collisionBlocks: any[];
 
 function setup() {
   backgroundSetup();
+  decorationsSetup();
   playersSetup();
+  collision();
+}
+
+function collision() {
+  for (
+    let index = 0;
+    index < CollisionBlockData.groundCollionBlockData.length;
+    index += 36
+  ) {
+    const element = CollisionBlockData.groundCollionBlockData.slice(
+      index,
+      index + 36
+    );
+    collisionBlocks.push(element);
+  }
 }
 
 function backgroundSetup() {
-  background = new Sprites(
-    ctx,
-    { w: ctx.canvas.width, h: ctx.canvas.height },
-    { x: 0, y: 0 },
-    "./img/Background/layer_1.png",
-    { top: 0, bottom: 0, left: 0, right: 0 }
-  );
+  background = new Sprites({
+    dimension: { w: ctx.canvas.width, h: ctx.canvas.height },
+    context: ctx,
+    // scale: 3,
+    position: { x: 0, y: 0 },
+    imgSrc: "./img/Background/layer_1.png",
+    frameTotal: 1,
+    sprites: {
+      standard: [{ imgSrc: "./img/Background/layer_1.png" }],
+    },
+  });
+
+  ground = new Sprites({
+    dimension: { w: ctx.canvas.width, h: ctx.canvas.height },
+    context: ctx,
+    // scale: 3,
+    position: { x: 0, y: 0 },
+    imgSrc: "./img/Background/ground.png",
+    frameTotal: 1,
+    sprites: {
+      standard: [{ imgSrc: "./img/Background/ground.png" }],
+    },
+  });
+}
+
+function decorationsSetup() {
+  shop = new Sprites({
+    context: ctx,
+    position: { x: 600, y: 104 },
+    scale: 2,
+    imgSrc: "./img/Decorations/Animated Decorations/shop.png",
+    frameTotal: 6,
+    sprites: {
+      standard: [{ imgSrc: "./img/Decorations/Animated Decorations/shop.png" }],
+    },
+  });
 }
 
 function playersSetup() {
-  player1 = new Player(
-    ctx,
-    { w: 64, h: 64 },
-    { x: 0, y: 0 },
-    "./img/Characters/knight/idle/idle_knight_1.png",
-    { top: 14, bottom: 10, left: 14, right: 30 },
-    { x: 0, y: 0 },
-     {
-            idle: [
-                { imgSrc: './img/Characters/knight/idle/idle_knight_1.png'},
-                { imgSrc: './img/Characters/knight/idle/idle_knight_2.png'},
-                { imgSrc: './img/Characters/knight/idle/idle_knight_3.png'},
-                { imgSrc: './img/Characters/knight/idle/idle_knight_4.png'},
-                { imgSrc: './img/Characters/knight/idle/idle_knight_5.png'},
-                { imgSrc: './img/Characters/knight/idle/idle_knight_6.png'}
-            ]
-        }
-  );
-  // this.player1 = new Player({
-  //     context: ctx,
-  //     dimension: {w: 64, h: 64},
-  //     offset: {top: 14, bottom: 10, left: 14, right: 30},
-  //     sprites: {
-  //         idle: [
-  //             { imgSrc: './img/Characters/knight/idle/idle_knight_1.png'},
-  //             { imgSrc: './img/Characters/knight/idle/idle_knight_2.png'},
-  //             { imgSrc: './img/Characters/knight/idle/idle_knight_3.png'},
-  //             { imgSrc: './img/Characters/knight/idle/idle_knight_4.png'},
-  //             { imgSrc: './img/Characters/knight/idle/idle_knight_5.png'},
-  //             { imgSrc: './img/Characters/knight/idle/idle_knight_6.png'}
-  //         ],
-  //         walk: [
-  //             { imgSrc: './img/Characters/knight/walk/walk_knight_1.png'},
-  //             { imgSrc: './img/Characters/knight/walk/walk_knight_2.png'},
-  //             { imgSrc: './img/Characters/knight/walk/walk_knight_3.png'},
-  //             { imgSrc: './img/Characters/knight/walk/walk_knight_4.png'},
-  //             { imgSrc: './img/Characters/knight/walk/walk_knight_5.png'},
-  //             { imgSrc: './img/Characters/knight/walk/walk_knight_6.png'},
-  //             { imgSrc: './img/Characters/knight/walk/walk_knight_7.png'},
-  //             { imgSrc: './img/Characters/knight/walk/walk_knight_8.png'},
-  //             { imgSrc: './img/Characters/knight/walk/walk_knight_8.png'},
-  //             { imgSrc: './img/Characters/knight/walk/walk_knight_10.png'},
-  //             { imgSrc: './img/Characters/knight/walk/walk_knight_11.png'},
-  //             { imgSrc: './img/Characters/knight/walk/walk_knight_12.png'}
-  //         ]
-  //     }
-  // })
+  player1 = new Player({
+    context: ctx,
+    position: { x: 0, y: 0 },
+    scale: 1.5,
+    imgSrc: "./img/Characters/Martial Hero/Sprites/Idle.png",
+    frameTotal: 8,
+    offset: { top: 94, bottom: 20, left: 75, right: 90 },
+    velocity: { x: 0, y: 0 },
+    sprites: {
+      idle: { imgSrc: "./img/Characters/Martial Hero/Sprites/Idle.png" },
+    },
+  });
 }
-
-// ctx.getImageData
-
-// ctx.getContextAttributes
-
-//ctx.fillRect(0,0,canvasWidth,canvasHeight)
 
 function gameRun() {
   window.requestAnimationFrame(gameRun);
+  console.log(Util.frameCounter);
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
   // checkColision(player1, ctx)
-  background.update();
+  background.draw();
+  ground.draw();
+  shop.update();
   player1.update();
 }
+
+window.addEventListener("keydown", (event) => {
+  Controller.controlKeyDown(event);
+});
+
+window.addEventListener("keyup", (event) => {
+  Controller.controlKeyUp(event);
+});
 
 setup();
 gameRun();
